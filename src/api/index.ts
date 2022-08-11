118c54d628a9c9e9da97704624af383088a37fd8
plump_albert <plumpalbert@gmail.com>
2022-08-11 23:22:31 +0300

    api: added error schema validator and type

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,14 @@
+import {z} from "zod";
+
 export const GITHUB_API_ENDPOINT = "https://api.github.com" as const;
+
+export const ErrorSchema = z.object({
+  /** Error message */
+  message: z.string(),
+  /** Github documentation page related to this problem */
+  documentation_url: z.string(),
+});
+
+export type ErrorType = z.infer<typeof ErrorSchema>;
+
+export * as repos from "./repos";
