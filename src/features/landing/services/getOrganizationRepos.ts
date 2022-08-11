eeecf2dc7595e7a1a70b421ff6f7f0e301d34496
plump_albert <plumpalbert@gmail.com>
2022-08-12 00:33:09 +0300

    api: renamed api folder to services

--- src/features/landing/services/getOrganizationRepos.ts
+++ src/features/landing/services/getOrganizationRepos.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
 import {z} from "zod";
 import {ErrorType, GITHUB_API_ENDPOINT} from "api";
+import {RepoSchema} from "../reposStore";
 
 export const getOrganizationRepos = async ({
   name,
   ...params
-}: z.infer<typeof ParameterSchema>) => {
+}: ParameterType) => {
   const response = await axios.get<ResponseType>(
     `${GITHUB_API_ENDPOINT}/orgs/${name}/repos`,
     {
       headers: {
         Accept: "application/vnd.github+json",
       },
-      params,
+      params: ParameterSchema.parse(params),
     }
   );
 
@@ -29,35 +30,21 @@ const ParameterSchema = z.object({
       invalid_type_error: "Field `name` must be a string!",
     })
     .min(1, {message: "Field `name` must be more than 1 character long!"}),
-  sort: z.enum(["created", "updated", "pushed", "full_name"]),
-  direction: z.enum(["desc", "asc"]),
+  sort: z.enum(["created", "updated", "pushed", "full_name"]).optional(),
+  direction: z.enum(["desc", "asc"]).optional(),
   per_page: z
     .number()
     .min(1, {message: "Field `per_page` must be greater than or equal to 1"})
     .max(100, {message: "Field `per_page` must be lower than 100"})
-    .default(30),
-  page: z.number().min(1, {message: "Field `page` must be greater than 1"}),
+    .default(30)
+    .optional(),
+  page: z
+    .number()
+    .min(1, {message: "Field `page` must be greater than 1"})
+    .optional(),
 });
+type ParameterType = z.infer<typeof ParameterSchema>;
 
-const ResponseSchema = z
-  .object({
-    /** Repository identifier */
-    id: z.number(),
-    /** Repository name */
-    name: z.string(),
-    /** Repository full name (organization/repo) */
-    full_name: z.string(),
-    /** Whether repository private or not */
-    private: z.boolean(),
-    /** URL of repository */
-    html_url: z.string(),
-    /** Repository description */
-    description: z.string(),
-    /** Main language of the repository */
-    language: z.string().nullable(),
-    /** Total number of stars for this repository */
-    stargazers_count: z.number(),
-  })
-  .array();
+const ResponseSchema = RepoSchema.array();
 
 type ResponseType = z.infer<typeof ResponseSchema> | ErrorType;
