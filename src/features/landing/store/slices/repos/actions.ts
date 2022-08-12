983f525ced7b5c5c1c859bf34a6520c6555b444d
plump_albert <plumpalbert@gmail.com>
2022-08-12 22:23:35 +0300

    format: realized that actionCreators are actually sagas

--- src/features/landing/store/slices/repos/actions.ts
+++ src/features/landing/store/slices/repos/actions.ts
@@ -1,34 +1,5 @@
-import {ErrorType} from "api";
-import {RepoState, RepoType, STORE_NAME} from ".";
+import {RepoState, STORE_NAME} from ".";
 import {Action, actionTypeCreator, ACTION_TYPE} from "app/actions";
-import * as fetchActionCreators from "../fetch/sagas";
-
-/** Action creator for handling fetch request of repos */
-export const fetchRepos = () =>
-  fetchActionCreators.fetchRequestSaga<RepoState>({
-    store: STORE_NAME,
-    property: "repos",
-  });
-
-/**
- * Action creator for handling response of repos fetch
- * @param repos - list of repos to append to store
- */
-export const fetchReposSuccess = (repos: RepoType[]) =>
-  fetchActionCreators.fetchSuccessSaga<RepoType[], RepoState>(
-    {store: STORE_NAME, property: "repos"},
-    repos
-  );
-
-/**
- * Action creator for handling errors that occurred while fetching repos
- * @param error - error object to set
- */
-export const fetchReposError = (error: ErrorType) =>
-  fetchActionCreators.fetchErrorSaga<ErrorType, RepoState>(
-    {store: STORE_NAME, property: "repos"},
-    error
-  );
 
 /**
  * Action creator for changing current organization name
---
+++ src/features/landing/store/slices/repos/sagas.ts
@@ -0,0 +1,51 @@
+import {call} from "@redux-saga/core/effects";
+import {getOrganizationRepos} from "../../../services/getOrganizationRepos";
+import {ErrorType} from "api";
+import * as fetchSagas from "../fetch/sagas";
+import {STORE_NAME, RepoState, RepoType} from ".";
+
+/** Saga for handling fetch request of repos */
+export const reposFetchStart = () =>
+  fetchSagas.fetchRequestSaga<RepoState>({
+    store: STORE_NAME,
+    property: "repos",
+  });
+
+/**
+ * Saga for handling response of repos fetch
+ * @param repos - list of repos to append to store
+ */
+export const reposFetchSuccess = (repos: RepoType[]) =>
+  fetchSagas.fetchSuccessSaga<RepoType[], RepoState>(
+    {store: STORE_NAME, property: "repos"},
+    repos
+  );
+
+/**
+ * Saga for handling errors that occurred while fetching repos
+ * @param error - error object to set
+ */
+export const reposFetchError = (error: ErrorType) =>
+  fetchSagas.fetchErrorSaga<ErrorType, RepoState>(
+    {store: STORE_NAME, property: "repos"},
+    error
+  );
+
+/**
+ * Saga for getting list of repositories using `params`
+ *
+ *  @param params
+ *  filters used for fetching repos
+ */
+function* getRepos(params: Parameters<typeof getOrganizationRepos>[0]) {
+  try {
+    yield call(reposFetchStart);
+    const repos: Awaited<ReturnType<typeof getOrganizationRepos>> = yield call(
+      getOrganizationRepos,
+      params
+    );
+    yield call(reposFetchSuccess, repos);
+  } catch (error: any) {
+    yield call(reposFetchError, error.message);
+  }
+}
