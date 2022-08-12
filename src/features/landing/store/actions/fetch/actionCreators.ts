90d641f058850618cbafdc2105d47c116a45cd16
plump_albert <plumpalbert@gmail.com>
2022-08-12 08:13:38 +0300

    store: implemented action creators for fetch-related tasks

---
+++ src/features/landing/store/actions/fetch/actionCreators.ts
@@ -0,0 +1,61 @@
+import {ACTION_TYPE, Action, GroupedAction} from "..";
+import {changeIsFetching, changeValue, setFetchError} from "./actions";
+
+/** Action creator for handling fetch requests */
+export const fetchRequest = ({
+  store,
+  property,
+  clearState,
+}: IFetchParams): GroupedAction => {
+  const path = {store, property};
+  const actions: Action[] = [changeIsFetching(path, false)];
+  if (clearState) {
+    actions.push(changeValue(path, {clear: true}));
+  }
+  return {
+    type: ACTION_TYPE.GROUPED,
+    payload: actions,
+  };
+};
+
+/** Action creator for handling fetch errors */
+export const fetchError = <T = any>(
+  {store, property, clearState}: IFetchParams,
+  error: T
+): GroupedAction => {
+  const path = {store, property};
+  const actions: Action[] = [changeIsFetching(path, false)];
+  if (clearState) {
+    actions.push(changeValue(path, {clear: true}));
+  }
+  actions.push(setFetchError(path, error));
+  return {
+    type: ACTION_TYPE.GROUPED,
+    payload: actions,
+  };
+};
+
+/** Action creator for handling fetch response */
+export const fetchSuccess = <T = any>(
+  {store, property, clearState}: IFetchParams,
+  value: T
+): GroupedAction => {
+  const path = {store, property};
+  const actions = [
+    changeIsFetching(path, false),
+    changeValue(path, {clear: !!clearState, value}),
+  ];
+  return {
+    type: ACTION_TYPE.GROUPED,
+    payload: actions,
+  };
+};
+
+interface IFetchParams {
+  /** Name of store to update */
+  store: string;
+  /** Path to `IFetchable` property */
+  property: string;
+  /** If `true` - clear current state */
+  clearState?: boolean;
+}
