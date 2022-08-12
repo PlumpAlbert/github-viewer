f7381a145dcd1a459365e746df5ee8df731ca7ba
plump_albert <plumpalbert@gmail.com>
2022-08-12 10:23:42 +0300

    store: created reducer for fetch slice

--- src/features/landing/store/actions/fetch/index.ts
+++ src/features/landing/store/actions/fetch/index.ts
@@ -1,3 +1,7 @@
+import {Reducer} from "@reduxjs/toolkit";
+import {ACTION_TYPE} from "..";
+import * as actions from "./actions";
+
 export interface IFetchable<T, E = any> {
   /** If `true` object value is currently been fetched */
   isFetching: boolean;
@@ -6,3 +10,39 @@ export interface IFetchable<T, E = any> {
   /** Fetch error state */
   fetchError?: E;
 }
+
+type ActionTypes = ReturnType<typeof actions[keyof typeof actions]>;
+
+const initialState: IFetchable<any> = {
+  isFetching: false,
+};
+
+const reducer: Reducer<IFetchable<any>, ActionTypes> = (
+  state = initialState,
+  action
+) => {
+  const {type, property} = action.type;
+  switch (type) {
+    case ACTION_TYPE.CHANGE: {
+      switch (property) {
+        case "isFetching": {
+          return {...state, isFetching: action.payload as boolean};
+        }
+        case "value": {
+          const {clear, value} = action.payload as actions.IClearable;
+          const newValue: any = clear ? value : {...state.value, ...value};
+          return {...state, value: newValue};
+        }
+        case "fetchError": {
+          return {...state, fetchError: action.payload};
+        }
+        default:
+          return state;
+      }
+    }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
