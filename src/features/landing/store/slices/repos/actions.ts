53ff2307420e9e9e2fe32a61eecee69384d33afd
plump_albert <plumpalbert@gmail.com>
2022-08-12 10:47:34 +0300

    store: created actions for repos slice

---
+++ src/features/landing/store/slices/repos/actions.ts
@@ -0,0 +1,42 @@
+import {ErrorType} from "api";
+import {RepoState, RepoType, STORE_NAME} from ".";
+import {Action, actionTypeCreator, ACTION_TYPE} from "../../actions";
+import * as fetchActionCreators from "../fetch/actionCreators";
+
+/** Action creator for handling fetch request of repos */
+export const fetchRepos = () =>
+  fetchActionCreators.fetchRequest<RepoState>({
+    store: STORE_NAME,
+    property: "repos",
+  });
+
+/**
+ * Action creator for handling response of repos fetch
+ * @param repos - list of repos to append to store
+ */
+export const fetchReposSuccess = (repos: RepoType[]) =>
+  fetchActionCreators.fetchSuccess<RepoType[], RepoState>(
+    {store: STORE_NAME, property: "repos"},
+    repos
+  );
+
+/**
+ * Action creator for handling errors that occurred while fetching repos
+ * @param error - error object to set
+ */
+export const fetchReposError = (error: ErrorType) =>
+  fetchActionCreators.fetchError<ErrorType, RepoState>(
+    {store: STORE_NAME, property: "repos"},
+    error
+  );
+
+/**
+ * Action creator for changing current organization name
+ * @param name - new name to set
+ */
+export const changeOrganizationName = (
+  name: string
+): Action<string, RepoState> => ({
+  type: actionTypeCreator(STORE_NAME, ACTION_TYPE.CHANGE, "name"),
+  payload: name,
+});
