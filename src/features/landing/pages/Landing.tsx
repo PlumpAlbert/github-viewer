6d785c0e1e721f2930f48a38e2269fc82f12b1fc
plump_albert <plumpalbert@gmail.com>
2022-08-13 17:32:58 +0300

    app: displaying Landing page

---
+++ src/features/landing/pages/Landing.tsx
@@ -0,0 +1,9 @@
+const LandingPage: React.FC = () => {
+  return (
+    <main className="flex flex-col">
+      <h1 className="text-4xl text-blue-600 font-semibold">Github Viewer</h1>
+    </main>
+  );
+};
+
+export default LandingPage;
