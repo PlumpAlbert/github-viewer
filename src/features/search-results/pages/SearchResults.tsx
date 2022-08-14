d6b7ff9def51a861cc4186202180dd5dc6d00b91
plump_albert <plumpalbert@gmail.com>
2022-08-14 09:02:14 +0300

    page/SearchResults: using `SearchReposField` in header

--- src/features/search-results/pages/SearchResults.tsx
+++ src/features/search-results/pages/SearchResults.tsx
@@ -1,7 +1,5 @@
-import {useState, useCallback} from "react";
-
-import {useAppDispatch, useAppSelector} from "app/hooks";
-import * as actions from "features/landing/store/slices/repos/actions";
+import {useAppSelector} from "app/hooks";
+import SearchReposField from "components/SearchReposField";
 
 import Table from "../components/Table";
 
@@ -10,44 +8,20 @@ const SearchResults: React.FC = () => {
     repoState: state.organization.repos,
     organizationName: state.organization.name,
   }));
-  const [inputValue, setInputValue] = useState(organizationName);
-
-  const dispatch = useAppDispatch();
-
-  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> =
-    useCallback(e => {
-      setInputValue(e.target.value);
-    }, []);
-
-  const handleSearchClick = useCallback(() => {
-    dispatch(actions.changeOrganizationName(inputValue));
-    dispatch(actions.fetchRepos({name: inputValue}));
-  }, [dispatch, inputValue]);
 
   return (
     <div className="flex flex-col w-full h-full">
-      <header className="flex gap-4 p-4 items-center bg-gray-100 dark:bg-gray-800">
-        <img className="h-10" src="/assets/images/github.png" />
-        <input
-          className={`${
-            repoState.fetchError && inputValue === organizationName
-              ? "error"
-              : ""
-          } rounded-full px-5 py-2.5`}
-          id="search-field"
-          aria-labelledby="search-field-label"
-          type="text"
-          placeholder="Enter organization name here..."
-          defaultValue={organizationName}
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-        <button className="icon-button" onClick={handleSearchClick}>
-          <i className="material-symbols-rounded">search</i>
-          <span className="sr-only"> Search </span>
-        </button>
+      <header className="flex gap-4 p-4 items-top bg-gray-100 dark:bg-gray-800">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="p-2 h-8 box-content fill-[#333] dark:fill-[#fafafa]"
+        >
+          <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+        </svg>
+        <SearchReposField value={organizationName} />
       </header>
-      {repoState.isFetching && (
+      {repoState.isFetching ? (
         <div className="m-auto" role="status">
           <svg
             aria-hidden="true"
@@ -67,8 +41,7 @@ const SearchResults: React.FC = () => {
           </svg>
           <span className="sr-only">Loading...</span>
         </div>
-      )}
-      {!repoState.value ? (
+      ) : !repoState.value ? (
         <main className="flex flex-col flex-1 gap-2">
           <h2 className="m-auto text-xl text-center p-4">
             Sorry, but nothing was found!
