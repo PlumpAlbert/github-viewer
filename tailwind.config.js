82440bf77a4a254faf99237e39331b19a4284dbd
plump_albert <plumpalbert@gmail.com>
2022-08-14 07:14:49 +0300

    style: added Inter font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 };
