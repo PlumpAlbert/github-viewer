f5b2da68a5dc30dd88c84c5f4a4bc534142426f3
plump_albert <plumpalbert@gmail.com>
2022-08-14 11:53:19 +0300

    page/Landing: added horizontal padding

--- src/features/landing/pages/Landing.tsx
+++ src/features/landing/pages/Landing.tsx
@@ -2,7 +2,7 @@ import SearchReposField from "components/SearchReposField";
 
 const LandingPage: React.FC = () => {
   return (
-    <main className="w-full h-full flex flex-col justify-center items-center gap-8 max-w-2xl m-auto">
+    <main className="w-full h-full flex flex-col justify-center items-center gap-8 max-w-2xl m-auto px-4">
       <h1 className="text-4xl text-blue-600 font-semibold uppercase dark:text-blue-400">
         Github Viewer
       </h1>
