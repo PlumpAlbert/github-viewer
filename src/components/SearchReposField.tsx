e72c5833a0bf6e247d722abb1505de3f999c37fc
plump_albert <plumpalbert@gmail.com>
2022-08-14 09:01:46 +0300

    page/Landing: created `SearchReposField` component

---
+++ src/components/SearchReposField.tsx
@@ -0,0 +1,73 @@
+import {useCallback, useState, useEffect} from "react";
+
+import {useAppSelector, useAppDispatch} from "app/hooks";
+import * as actions from "features/landing/store/slices/repos/actions";
+
+const SearchReposField: React.FC<SearchReposFieldProps> = ({value = ""}) => {
+  const {organizationName, error} = useAppSelector(state => ({
+    organizationName: state.organization.name,
+    error: state.organization.repos.fetchError,
+  }));
+  const [inputValue, setInputValue] = useState(value || organizationName);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (error && inputValue !== organizationName) {
+      dispatch(actions.clearError());
+    }
+  }, [inputValue]);
+
+  const handleFormSubmit: React.FormEventHandler = useCallback(
+    e => {
+      e.preventDefault();
+      dispatch(actions.changeOrganizationName(inputValue));
+      dispatch(actions.fetchRepos({name: inputValue}));
+    },
+    [dispatch, inputValue]
+  );
+
+  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback(e => {
+      const value = e.currentTarget.value;
+      setInputValue(value);
+    }, []);
+
+  return (
+    <form
+      action=""
+      className="w-full flex gap-4 items-start"
+      onSubmit={handleFormSubmit}
+    >
+      <div className="flex flex-col flex-1 gap-2">
+        <input
+          id="search-field"
+          aria-labelledby="search-field-label"
+          type="text"
+          placeholder="Enter organization name here..."
+          className={error && "error"}
+          onChange={handleInputChange}
+          value={inputValue}
+        />
+        {error && (
+          <p className="flex items-center gap-1 px-3 text-sm text-red-500 dark:text-red-400">
+            <i className="material-symbols-rounded w-6 h-6 text-center text-base">
+              error
+            </i>
+            {error}
+          </p>
+        )}
+      </div>
+      <button type="submit" className="icon-button">
+        <i className="material-symbols-rounded">search</i>
+        <span className="sr-only"> Search </span>
+      </button>
+    </form>
+  );
+};
+
+export default SearchReposField;
+
+type SearchReposFieldProps = {
+  value?: string;
+};
--- src/features/landing/pages/Landing.tsx
+++ src/features/landing/pages/Landing.tsx
@@ -1,77 +1,12 @@
-import {useCallback, useEffect, useState} from "react";
-
-import {useAppDispatch, useAppSelector} from "app/hooks";
-
-import * as actions from "../store/slices/repos/actions";
+import SearchReposField from "components/SearchReposField";
 
 const LandingPage: React.FC = () => {
-  const [inputValue, setInputValue] = useState("");
-  const {organizationName, error} = useAppSelector(state => ({
-    organizationName: state.organization.name,
-    error: state.organization.repos.fetchError,
-  }));
-
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (error && inputValue !== organizationName) {
-      dispatch(actions.clearError());
-    }
-  }, [inputValue]);
-
-  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> =
-    useCallback(
-      e => {
-        const value = e.currentTarget.value;
-        setInputValue(value);
-      },
-      [dispatch]
-    );
-
-  const handleSearchClick: React.MouseEventHandler<HTMLButtonElement> =
-    useCallback(() => {
-      dispatch(actions.changeOrganizationName(inputValue));
-      dispatch(actions.fetchRepos({name: inputValue}));
-    }, [dispatch, inputValue]);
-
   return (
     <main className="w-full h-full flex flex-col justify-center items-center gap-8 max-w-2xl m-auto">
       <h1 className="text-4xl text-blue-600 font-semibold uppercase dark:text-blue-400">
         Github Viewer
       </h1>
-      <form
-        action=""
-        className="w-full flex gap-4 items-start"
-        onSubmit={e => e.preventDefault()}
-      >
-        <div className="flex flex-col flex-1 gap-2">
-          <input
-            id="search-field"
-            aria-labelledby="search-field-label"
-            type="text"
-            placeholder="Enter organization name here..."
-            className={error && "error"}
-            onChange={handleInputChange}
-            value={inputValue}
-          />
-          {error && (
-            <p className="flex items-center gap-1 px-3 text-sm text-red-500 dark:text-red-400">
-              <i className="material-symbols-rounded w-6 h-6 text-center text-base">
-                error
-              </i>
-              {error}
-            </p>
-          )}
-        </div>
-        <button
-          type="submit"
-          className="icon-button"
-          onClick={handleSearchClick}
-        >
-          <i className="material-symbols-rounded">search</i>
-          <span className="sr-only"> Search </span>
-        </button>
-      </form>
+      <SearchReposField />
     </main>
   );
 };
